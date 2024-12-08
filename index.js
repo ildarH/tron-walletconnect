6a9d22ad4fcef2e6ae2fa3a503ce3d934395f443
ildarH <ildar.habibrahmanov@icloud.com>
2024-12-08 09:54:05 +0500

    init commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+
+require('dotenv').config(); // Load environment variables
+
+const express = require('express');
+const app = express();
+const port = 3001;
+
+const crypto = require('crypto');
+const ethers = require('ethers');
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { TronWeb } = require('tronweb');
+
+const { createHmac } = require('crypto');
+
+const tronWeb = new TronWeb({
+    fullHost: 'https://api.trongrid.io',
+});
