0e737dec2ff63fa7bad613dac874e45f4b7b035e
ildarH <86957485+ildarH@users.noreply.github.com>
2024-12-16 20:27:00 +0500

    Dev (#5)
    * add frontend
    
    * bump tronweb
    
    * add verify method
    
    * refactor
    
    * add copy
    
    * deployment script
    
    * Update BASE_URL in render.yaml and organize package.json dependencies
    
    * Refactor backend configuration and module imports for ES module compatibility
    
    - Updated package.json scripts to include experimental specifier resolution for TypeScript.
    - Changed TypeScript compiler options to use NodeNext module resolution.
    - Added ts-node configuration for ESM support.
    - Modified import statements across multiple files to include the '.js' extension for ES module compatibility.
    - Updated dependencies in package.json to include ts-node and nodemon.
    
    These changes enhance the backend's compatibility with ES modules and improve the development workflow.
    
    * Enhance Vite configuration for improved module resolution and polyfills
    
    - Updated Vite plugins to include specific polyfills for 'buffer', 'crypto', 'util', and 'stream'.
    - Added CommonJS options to support mixed ES module transformations.
    - Configured aliasing for 'process', 'stream', and 'util' to their browser-compatible versions.
    - Defined global variables for process and global in the build.
    - Optimized dependencies with esbuild options to define 'global' as 'globalThis'.
    
    These changes improve compatibility with browser environments and streamline the build process.
    
    * Update frontend dependencies and enhance Vite configuration
    
    - Added 'axios' version 1.6.2 to package.json and package-lock.json.
    - Introduced '@rollup/plugin-commonjs' version 25.0.0 as a new devDependency.
    - Updated Vite configuration to include CommonJS plugin with options for mixed ES module transformations and to ignore 'axios'.
    - Improved compatibility with browser environments by configuring polyfills and optimizing dependency handling.
    
    These changes streamline the frontend build process and ensure better module resolution.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import commonjs from '@rollup/plugin-commonjs'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [
+    react(), 
+    nodePolyfills({
+      include: ['buffer', 'crypto', 'util', 'stream'],
+    }),
+    commonjs({
+      transformMixedEsModules: true,
+      ignore: ['axios']
+    }),
+  ],
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  resolve: {
+    alias: {
+      process: 'process/browser',
+      stream: 'stream-browserify',
+      util: 'util',
+      zlib: 'browserify-zlib',
+    },
+  },
+  define: {
+    'process.env': {},
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+    },
+    include: [],
   },
   server: {
     host: true,
