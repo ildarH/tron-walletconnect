ceb51c0e7c28cf9814a098372a03ead9acb1455f
ildarH <ildar.habibrahmanov@icloud.com>
2024-12-17 22:17:15 +0500

    Update Vite configuration and .gitignore
    - Removed unused CommonJS plugin and related configurations from vite.config.ts to simplify the build process.
    - Updated .gitignore to exclude '.dist' files, ensuring cleaner project structure.
    
    These changes enhance the frontend's build configuration and improve file management.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,48 +1,9 @@
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
-import commonjs from '@rollup/plugin-commonjs'
+// import commonjs from '@rollup/plugin-commonjs'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    nodePolyfills({
-      include: ['buffer', 'crypto', 'util', 'stream', 'vm'],
-    }),
-    commonjs({
-      transformMixedEsModules: true,
-      ignore: ['axios']
-    }),
-  ],
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-  resolve: {
-    alias: {
-      process: 'process/browser',
-      stream: 'stream-browserify',
-      util: 'util',
-      zlib: 'browserify-zlib',
-    },
-  },
-  define: {
-    'process.env': {},
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-    },
-    include: [],
-  },
-  server: {
-    host: true,
-    port: 3000
-  }
+  plugins: [react(), nodePolyfills()],
 });
